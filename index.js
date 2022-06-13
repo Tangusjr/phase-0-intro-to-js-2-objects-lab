a57ee29f9e313117bae977f1781342cd9e0491ae
tangusjr <kipngenogilbert96@gmail.com>
2022-06-13 22:13:32 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+const employee = {
+  name: "tangus",
+  streetAddress: "kericho-kisumu",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, [key]: value };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const copyEmployee = { ...employee };
+  delete copyEmployee[key];
+  return copyEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
